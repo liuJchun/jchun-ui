0f2dc96392623eb05c27d9b4b8e049031e9d40c3
liujianchun <943309727@qq.com>
2023-02-15 17:41:15 +0800

    feat：增加transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 // import logo from "./logo.svg"
-import "./App.css"
+import './App.css'
 
-import Menu from "./packages/Menu"
-import MenuItem from "./packages/Menu/MenuItem"
-import SubMenu from "./packages/Menu/SubMenu"
-import Icon from "./packages/Icon"
+import Menu from './packages/Menu'
+import MenuItem from './packages/Menu/MenuItem'
+import SubMenu from './packages/Menu/SubMenu'
+import Icon from './packages/Icon'
 
 function App() {
     return (
         <div className="App">
             <div>
                 <Icon icon="coffee" size="10x" theme="danger"></Icon>
-
                 <h1>Menu</h1>
                 <Menu>
                     <MenuItem index={0} disabled>
@@ -26,20 +25,6 @@ function App() {
                     </SubMenu>
                     <MenuItem index={1}>123</MenuItem>
                     <MenuItem index={2}>1eee23</MenuItem>
-                </Menu>
-                <Menu mode="vertical">
-                    <MenuItem index={0} disabled>
-                        123
-                    </MenuItem>
-                    <MenuItem index={1}>123</MenuItem>
-                    <MenuItem index={2}>1eee23</MenuItem>
-                    <SubMenu title="sub-menu">
-                        <MenuItem index={5}>5</MenuItem>
-                        <MenuItem index={6}>6</MenuItem>
-                        <MenuItem index={7}>7</MenuItem>
-                        <MenuItem index={8}>8</MenuItem>
-                        <MenuItem index={9}>9</MenuItem>
-                    </SubMenu>
                 </Menu>
             </div>
         </div>
--- src/packages/Transition/Transition.tsx
+++ src/packages/Transition/Transition.tsx
@@ -1,8 +1,9 @@
-import { CSSTransition } from "react-transition-group"
-import { CSSTransitionProps } from "react-transition-group/CSSTransition"
-import { FC } from "react"
+import { CSSTransition } from 'react-transition-group'
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
+import { FC } from 'react'
 
-type animationType = "zoom-in-top" | "zoom-in-left" | "zoom-in-bottom" | "zoom-in-right"
+type animationType = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right'
+
 export type TransitionProps = CSSTransitionProps & {
     children?: React.ReactNode
     animation?: animationType
@@ -15,7 +16,6 @@ const Transition: FC<TransitionProps> = props => {
     return (
         <CSSTransition classNames={classNames || animation} {...restProps}>
             {wrapper ? <div>{children}</div> : children}
-            {/* <div>{children}</div> */}
         </CSSTransition>
     )
 }
@@ -23,5 +23,6 @@ const Transition: FC<TransitionProps> = props => {
 Transition.defaultProps = {
     appear: true,
     unmountOnExit: true,
+    wrapper: false,
 }
 export default Transition
